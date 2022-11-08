f462c334dc31e6c56b53bc68175998240942c905
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-08 17:09:29 +0000

    Refactored make_snippet

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,8 +1,4 @@
 def make_snippet(string)
   words = string.split(' ')
-  result = words[(0..4)].join(' ')
-  if words.count > 5
-    result += "..."
-  end
-  return result
+  words.count > 5 ? words[(0..4)].join(' ') + "..." : string
 end
