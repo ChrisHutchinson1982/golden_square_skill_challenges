6eb0c7d4927d4dedfb3201fc9ba9405f1a1daa3f
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-10 15:24:50 +0000

    Finialised tests and code for GrammarStats class

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,15 +1,25 @@
 class GrammarStats
   def initialize
-    # ...
+    @scores_array = []
   end
 
   def check(text) # text is a string
     # Returns true or false depending on whether the text begins with a capital
     # letter and ends with a sentence-ending punctuation mark.
+    
+    first_letter = text[0]
+    last_char = text[-1]
+
+    output = (first_letter == first_letter.capitalize && last_char.match(/[.?!]/) ? true : false)
+    @scores_array << output
+    return output
+
   end
 
   def percentage_good
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
+    pass_scores = (@scores_array.count(true).to_f/@scores_array.count) * 100
+    pass_scores.round
   end
 end
---
+++ spec/grammar_stats_spec.rb
@@ -0,0 +1,58 @@
+require "grammar_stats"
+
+RSpec.describe GrammarStats do
+
+  context "returns true" do
+    it "when string is 'Hello.'" do
+      string = GrammarStats.new
+      expect(string.check("Hello.")).to eq true
+    end
+
+    it "when string is 'Hello?'" do
+      string = GrammarStats.new
+      expect(string.check("Hello?")).to eq true
+    end
+
+    it "when string is 'Hello!'" do
+      string = GrammarStats.new
+      expect(string.check("Hello!")).to eq true
+    end
+ 
+  end
+
+  context "returns false" do
+    it "when string is 'hello.'" do
+      string = GrammarStats.new
+      expect(string.check("hello.")).to eq false
+    end
+
+    it "when string is 'Hello'" do
+      string = GrammarStats.new
+      expect(string.check("Hello")).to eq false
+    end
+  end
+
+  context "returns % of passed test" do
+    it "when 1 tests is pass return 100" do
+      string = GrammarStats.new
+      string.check("Hello?")
+      expect(string.percentage_good).to eq 100
+    end  
+    it "when 1 tests is pass and other is fail return 50" do
+      string = GrammarStats.new
+      string.check("Hello?")
+      string.check("Hello")
+      expect(string.percentage_good).to eq 50
+    end 
+    
+    it "when 2 tests is pass and other is fail return 50" do
+      string = GrammarStats.new
+      string.check("Hello?")
+      string.check("Hello?")
+      string.check("Hello")
+      expect(string.percentage_good).to eq 67
+    end  
+
+  end
+
+end
