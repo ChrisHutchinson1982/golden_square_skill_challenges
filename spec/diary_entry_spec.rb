0827fd7c97e128ba4cab51fdd6b55a1ddb3a6264
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-10 12:55:06 +0000

    Added initial tests and code for DiaryEntry class

---
+++ spec/diary_entry_spec.rb
@@ -0,0 +1,22 @@
+require 'diary_entry'
+
+RSpec.describe DiaryEntry do
+  
+  it "adds and returns a string of the title" do
+    diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+    expect(diary_entry.title).to eq "Today"
+  end
+
+  it "adds and returns a string contents" do
+    diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+    expect(diary_entry.contents).to eq "I am feeling great!"
+  end
+
+  it "adds and returns number of words of contents" do
+    diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+    expect(diary_entry.count_words). to eq 4
+  end
+
+  
+
+end
