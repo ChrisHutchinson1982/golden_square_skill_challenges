61b6b967020dca3bd22ee4660d76056f3e50f59d
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-12 10:40:54 +0000

    Refactored diary_entry class and test

--- lib/05_test_drive_a_class/diary_entry.rb
+++ lib/05_test_drive_a_class/diary_entry.rb
@@ -25,6 +25,7 @@ class DiaryEntry
     # user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # for the contents at the given wpm.
+    fail "Error, wpm cannot be 0 or nil" if wpm == 0 || wpm == nil
     words_per_min = @contents.split(" ").count.to_f / wpm
     words_per_min.round
   end
@@ -40,14 +41,14 @@ class DiaryEntry
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning.
 
-    return "" if wpm == 0 || minutes == 0
+    fail "Error, wpm or minutes cannot be 0 or nil" if wpm == 0 || minutes == 0 || wpm == nil || minutes == nil
     
     number_of_words = (wpm * minutes)
 
     output = @contents.split(" ")[@starting_index...(@starting_index + number_of_words)].join(" ")
     
     @starting_index += number_of_words
-    @starting_index = 0 if @starting_index > @contents.split(" ").count
+    @starting_index = 0 if @starting_index >= @contents.split(" ").count
 
     return output
   end
--- spec/05_test_drive_a_class/diary_entry_spec.rb
+++ spec/05_test_drive_a_class/diary_entry_spec.rb
@@ -12,45 +12,85 @@ RSpec.describe DiaryEntry do
     expect(diary_entry.contents).to eq "I am feeling great!"
   end
 
-  it "adds and returns number of words of contents" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great!")
-    expect(diary_entry.count_words). to eq 4
+  describe "@count_words" do
+    it "returns number of words of contents" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+      expect(diary_entry.count_words). to eq 4
+    end
+
+    it "returns 0 when of contents when empty" do
+      diary_entry = DiaryEntry.new("Today", "")
+      expect(diary_entry.count_words). to eq 0
+    end
   end
 
-  it "adds and returns estimated wpm user can read" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great!")
-    expect(diary_entry.reading_time(1)).to eq 4
-  end
+  describe "@reading_time" do
+    it "returns estimated wpm user can read" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+      expect(diary_entry.reading_time(1)).to eq 4
+    end
 
-  it "add and returns reading_chunk from contents that user can read in a minute" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
-    expect(diary_entry.reading_chunk(1, 1)).to eq "I"
-  end
+    it "returns 0 when of contents when empty" do
+      diary_entry = DiaryEntry.new("Today", "")
+      expect(diary_entry.reading_time(1)).to eq 0
+    end
 
-  it "add and returns "" if wpm or minutes is 0" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
-    expect(diary_entry.reading_chunk(0, 0)).to eq ""
-  end
+    it "returns error when of wpm when empty" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+      expect { diary_entry.reading_time(0) }.to raise_error "Error, wpm cannot be 0 or nil"
+    end
 
-  it "add and returns second reading_chunk from contents that user can read in a minute" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
-    diary_entry.reading_chunk(1,1)
-    expect(diary_entry.reading_chunk(2,1)).to eq "am feeling"
-  end
+    it "returns error when of wpm when nil" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great!")
+      expect { diary_entry.reading_time(nil) }.to raise_error "Error, wpm cannot be 0 or nil"
+    end
+    
+  end 
 
-  it "dd and returns third reading_chunk from contents that user can read in a minute" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
-    diary_entry.reading_chunk(4,1)
-    diary_entry.reading_chunk(3,1)
-    expect(diary_entry.reading_chunk(4,1)).to eq "tricky!"
-  end
+  describe "@reading_chunk" do
+    it "add and returns reading_chunk from contents that user can read in a minute" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      expect(diary_entry.reading_chunk(1, 1)).to eq "I"
+    end
 
-  it "returns reading_chunk to start if contents fully read" do
-    diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
-    diary_entry.reading_chunk(4,1)
-    diary_entry.reading_chunk(3,1)
-    diary_entry.reading_chunk(4,1)
-    expect(diary_entry.reading_chunk(1,1)).to eq "I"
+    it "returns error if wpm or minutes is 0" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      expect { diary_entry.reading_chunk(0, 0) }.to raise_error "Error, wpm or minutes cannot be 0 or nil"
+    end
+
+    it "returns error if wpm or minutes is nil" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      expect { diary_entry.reading_chunk(nil, nil) }.to raise_error "Error, wpm or minutes cannot be 0 or nil"
+    end
+
+    it "add and returns second reading_chunk from contents that user can read in a minute" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      diary_entry.reading_chunk(1,1)
+      expect(diary_entry.reading_chunk(2,1)).to eq "am feeling"
+    end
+
+    it "dd and returns third reading_chunk from contents that user can read in a minute" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      diary_entry.reading_chunk(4,1)
+      diary_entry.reading_chunk(3,1)
+      expect(diary_entry.reading_chunk(4,1)).to eq "tricky!"
+    end
+
+    it "returns reading_chunk to start if contents more than fully read" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      diary_entry.reading_chunk(4,1)
+      diary_entry.reading_chunk(3,1)
+      diary_entry.reading_chunk(4,1)
+      expect(diary_entry.reading_chunk(1,1)).to eq "I"
+    end
+
+    it "returns reading_chunk to start if contents equal to fully read" do
+      diary_entry = DiaryEntry.new("Today", "I am feeling great but think sounds tricky!")
+      diary_entry.reading_chunk(4,1)
+      diary_entry.reading_chunk(3,1)
+      diary_entry.reading_chunk(1,1)
+      expect(diary_entry.reading_chunk(1,1)).to eq "I"
+    end
   end
 
 end
