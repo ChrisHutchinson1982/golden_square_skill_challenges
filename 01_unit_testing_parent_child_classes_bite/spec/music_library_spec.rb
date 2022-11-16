b6e49c0ee25cf51a12a874732d47c6a8d86f274c
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-16 16:01:32 +0000

    Created and passed mocking tests for MusicLibrary class

--- 01_unit_testing_parent_child_classes_bite/spec/music_library_spec.rb
+++ 01_unit_testing_parent_child_classes_bite/spec/music_library_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe MusicLibrary do
     music_library.add(fake_track_2)
     expect(music_library.all).to eq [fake_track_1, fake_track_2]
   end
-  
+  it "adds and list all tracks with keyword" do
+    music_library = MusicLibrary.new
+    fake_track_1 = double :track, keyword: "keyword"
+    fake_track_2 = double :track, keyword: "keyword"
+    fake_track_2 = double :track, keyword: "not keyword"
+    music_library.add(fake_track_1)
+    music_library.add(fake_track_2)
+    expect(music_library.search("keyword")).to eq [fake_track_1, fake_track_2]
+  end    
 end
